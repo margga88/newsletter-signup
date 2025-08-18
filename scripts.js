0c6c074c3e6090e940cbde84d8e5ed4a4f1c3ca5
margga88 <margga88@yandex.com>
2025-08-18 15:50:58 -0400

    Fifth color fix

--- scripts.js
+++ scripts.js
@@ -42,7 +42,7 @@ function backHome(e) {
         cardOne.style.display = 'inline-flex';
         cardTwo.style.display = 'none';
         emailError.style.display = 'none';
-        emailInput.style.color = '';        
+        emailInput.style.color = '#333';                    
         emailInput.style.borderColor = 'var(--neutral1)';
         emailInput.style.backgroundColor = 'var(--neutral2)';
     }  
